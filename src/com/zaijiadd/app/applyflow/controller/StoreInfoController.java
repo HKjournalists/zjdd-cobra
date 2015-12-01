6062aea0f68c5a0ddb88e2e7fb5da019b062e2cb
guofengcug <admin@admin-PC>
2015-12-01 20:43:44 +0800

    数据库切换

--- src/com/zaijiadd/app/applyflow/controller/StoreInfoController.java
+++ src/com/zaijiadd/app/applyflow/controller/StoreInfoController.java
@@ -3,20 +3,15 @@ package com.zaijiadd.app.applyflow.controller;
 import java.util.HashMap;
 import java.util.Map;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.alibaba.fastjson.JSONObject;
-import com.zaijiadd.app.applyflow.entity.InviteUserEntity;
 import com.zaijiadd.app.applyflow.entity.StoreInfo;
 import com.zaijiadd.app.applyflow.service.StoreInfoService;
 import com.zaijiadd.app.common.utils.ContainerUtils;
-import com.zaijiadd.app.common.utils.ParseUtils;
 
 /**
  * 店铺开户上架入口
