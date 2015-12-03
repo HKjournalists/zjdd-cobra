385a5f3955c11ccace7d83d220504dc085004b8f
guofengcug <admin@admin-PC>
2015-12-03 17:09:40 +0800

    小区数据导入

---
+++ src/com/zaijiadd/app/applyflow/dao/CommunityMapper.java
@@ -0,0 +1,17 @@
+package com.zaijiadd.app.applyflow.dao;
+
+import com.zaijiadd.app.applyflow.entity.Community;
+
+public interface CommunityMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Community record);
+
+    int insertSelective(Community record);
+
+    Community selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Community record);
+
+    int updateByPrimaryKey(Community record);
+}
