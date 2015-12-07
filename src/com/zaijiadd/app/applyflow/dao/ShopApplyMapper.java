ce86356420932181503c7eb874bbee209a5bafee
guofengcug <guofengcug@163.com>
2015-12-07 21:12:41 +0800

    用户名密码在开店的时候加进去

--- src/com/zaijiadd/app/applyflow/dao/ShopApplyMapper.java
+++ src/com/zaijiadd/app/applyflow/dao/ShopApplyMapper.java
@@ -1,5 +1,6 @@
 package com.zaijiadd.app.applyflow.dao;
 
+import com.zaijiadd.app.applyflow.dto.ShopVO;
 import com.zaijiadd.app.applyflow.entity.ShopApply;
 
 public interface ShopApplyMapper {
@@ -11,6 +12,8 @@ public interface ShopApplyMapper {
     int insertSelective(ShopApply record);
 
     ShopApply selectByPrimaryKey(Long shopId);
+    
+    ShopVO selectByShopId(Long shopId);
 
     int updateByPrimaryKeySelective(ShopApply record);
 
